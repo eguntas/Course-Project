0ef1e9d53baa000117415f61a731fe46d57b0c0b
ercan gun <e.guntas@windowslive.com>
2026-04-01 15:19:48 +0300

    Build WEB UI Project

---
+++ Course.Web/Extensions/OptionsExtension.cs
@@ -0,0 +1,17 @@
+﻿using Course.Web.Options;
+using Microsoft.Extensions.Options;
+
+namespace Course.Web.Extensions
+{
+    public static class OptionsExtension
+    {
+        public static IServiceCollection AddOptionExt(this IServiceCollection services) 
+        {
+            services.AddOptions<IdentityOption>().BindConfiguration(nameof(IdentityOption)).ValidateDataAnnotations().ValidateOnStart();
+            
+            services.AddSingleton<IdentityOption>(sp => sp.GetRequiredService<IOptions<IdentityOption>>().Value);
+
+            return services;
+        }
+    }
+}
---
+++ Course.Web/Pages/Auth/SignUp/SignUpViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Course.Web.Pages.Auth.SignUp
+{
+    public record SignUpViewModel(
+        [Display(Name ="Username" )] string Username ,
+        [Display(Name = "First name")] string FirstName ,
+        [Display(Name = "Last name")] string LastName ,
+        [Display(Name = "Email")] string Email ,
+        [Display(Name = "Password")] string Password ,
+        [Display(Name = "Password Confirm")] string PasswordConfirm)
+    {
+        public static SignUpViewModel Empty => new(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
+
+        public static SignUpViewModel ExampleMdodel => new("TestUser", "Test", "User", "testuser@test.com", "Password123", "Password123");
+    }
+
+
+    
+}
